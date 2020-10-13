e7344d54bea0fe9ed4f0bb358a92445fd3fe21dd
Ford St. John <fstjohn@onemagnify.com>
2020-10-12 19:16:42 -0700

    initial setup of web interface displaying welcome msg

--- nfl_site/response/views.py
+++ nfl_site/response/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    my_dict = {'insert_me':"Now I am coming from response/index.html"}
+    return render(request, 'response/index.html', context=my_dict) # Uses the index.html template with the my_dict variable
