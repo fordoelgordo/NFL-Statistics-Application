10905b9f49d18b7b64f8d45c9361b8c46258d995
Kyle Dean <kdean003@ucr.edu>
2020-10-22 00:22:44 -0700

    updated forms for passing app

--- nfl_site/passing/forms.py
+++ nfl_site/passing/forms.py
@@ -14,7 +14,7 @@ PASSING_OUTCOME = [
     ]
 
 class PassingForm(forms.Form):
-    player_name = forms.CharField(label = 'Enter Player\'s Name', required = False)
-    passing_year = forms.IntegerField(label = 'Select Year', required = False, widget = forms.Select(choices=PASSING_YEARS))
-    passing_outcome = forms.CharField(label = 'Select Passing Outcome', required = False, widget = forms.Select(choices=PASSING_OUTCOME))
-    passing_length = forms.IntegerField(label = 'Enter Passing Length (Yards)', required = False)
+    player_name = forms.CharField(label = 'Player\'s Name', required = False)
+    passing_year = forms.IntegerField(label = 'Year', required = False, widget = forms.Select(choices=PASSING_YEARS))
+    passing_outcome = forms.CharField(label = 'Passing Outcome', required = False, widget = forms.Select(choices=PASSING_OUTCOME))
+    passing_length = forms.IntegerField(label = 'Passing Length (Yards)', required = False)
