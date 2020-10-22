ab2949fb1e63d731dd82fbe0a2b3c274c4c35710
Eduardo Rocha <eduardor626@yahoo.com>
2020-10-21 22:24:08 -0700

    finished up sprint2 for rushers

--- nfl_site/rushers/views.py
+++ nfl_site/rushers/views.py
@@ -1,8 +1,106 @@
 from django.shortcuts import render
 import pandas as pd
-from rushers import forms
+from .forms import RushersForm
+
+
 # Create your views here.
 
 def rusher_page(request):
-    print('in rushers page!!!!')
-    return render(request, 'rushers/rusher.html')
+    submitbutton = request.POST.get("submit")
+    # store players id and rushing yards
+    player_dict = {}  
+    player_name = ''
+    player_team = []
+
+    #check if form has been clicked or not
+    form = RushersForm(request.POST or None)
+
+    if form.is_valid():
+        player_dict.clear()  # clear info from previous search
+        player_name = form.cleaned_data.get("player_name")  # get player name from form
+        split_name = player_name.split()  # split input to get firstname and lastname
+
+        # load players csv from dataset
+        df_players = readPlayers()
+
+        # load rushers csv from dataset
+        df_rusher = readRushers()
+
+        #load teams csv from dataset
+        df_teams = readTeams()
+
+        # filter out players based on the name entered
+        name_filter = readPlayerName(df_players,split_name)
+        
+        if len(name_filter) == 0:
+            # if player does not exist set tuple to false and empty string
+            temp_tup = False, []
+        else:
+            # if player does exist return list of id's of all players with the same name
+            player_id_list = name_filter['playerId'].tolist()
+            temp_tup = True, player_id_list
+
+        if not temp_tup[0]:
+            # if first value in the tuple is false the name entered does nott exist in data set
+            player_name = 'Does not exist in data set'
+            player_dict = []
+        else:
+
+            # for each player id in the player id list (the second value in temp tup) find all occuences of the player
+            # id in the receiver csv
+            for player_id in temp_tup[1]:
+
+                # get all rusher yards for playerId as long as they exist and were not overturned
+                rushers_filter \
+                    = df_rusher.loc[(df_rusher['playerId'] == player_id) & (df_rusher['rushNull'] == 0)]
+
+
+                # Note players are added to a python dictionary with id as key and value bwing the total rusher yards
+                # i.e. player_dict[player_id] = Total rushing yards
+                if len(rushers_filter) == 0:
+                    # if no results found player has zero receiving yards
+                    player_dict[player_id] = 0
+                else:
+                    # sum the rushYards row of the data frame
+                    total_rush_yards = rushers_filter['rushYards'].sum()
+
+                    player_dict[player_id] = int(total_rush_yards)
+                    player_team_id = getPlayerTeam(player_id)
+                    player_team = getTeamName(player_team_id)
+                    # print(player_dict)
+
+
+    context = {'form': form, 'player_name': player_name, 'player_dict': player_dict, 'submit_button': submitbutton, 'player_team': player_team}
+
+    return render(request, 'rushers/rusher.html', context)
+
+
+def readPlayers():
+    return pd.read_csv("/Users/eduardorocha/workspace/IntroToSWE/players.csv")
+
+def readRushers():
+    return pd.read_csv("/Users/eduardorocha/workspace/IntroToSWE/rusher.csv")
+
+
+def readPlayerName(df_players,split_name):
+    return df_players.loc[(df_players['nameFirst'] == split_name[0]) & (df_players['nameLast'] == split_name[1])]
+
+def readTeams():
+    df = pd.read_csv("/Users/eduardorocha/workspace/IntroToSWE/draft.csv")
+    team_df = df[['teamId','draftTeam']].drop_duplicates()
+    return team_df
+
+def getPlayerTeam(player_id):
+    df = readRushers()
+    filter_df = df.loc[(df['playerId'] == player_id)]
+    get_team = filter_df['teamId'].drop_duplicates().tolist()
+    return get_team
+
+def getTeamName(team_id):
+    df = readTeams()
+    team_names = []
+    for i in team_id:
+        team_name = df[df['teamId'] == i]['draftTeam'].unique().tolist()
+        team_names.append(team_name)
+    print(team_names)
+    return team_names
