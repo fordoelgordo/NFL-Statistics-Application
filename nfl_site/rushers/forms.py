e1567405b5d6c1c300da8e1b25f3f3a2f904a421
Eduardo Rocha <eduardor626@yahoo.com>
2020-11-10 07:41:19 -0800

    fixed form side by side

--- nfl_site/rushers/forms.py
+++ nfl_site/rushers/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
-FRUIT_CHOICES= [('all time','All Time'),]
+TEAM_CHOICES= [('all time','All Time'),]
 
 class RushersForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
+    first_name = forms.CharField(label = 'First Name', max_length=100,required=True)
+    last_name = forms.CharField(label = 'Last Name', max_length=100,required=True)
 
 class TeamPickForm(forms.Form):
-    team_name = forms.CharField(widget=forms.Select(choices=FRUIT_CHOICES) )
+    team_name = forms.CharField(label = 'Top Rushers',required = False, widget=forms.Select(choices=TEAM_CHOICES) )
