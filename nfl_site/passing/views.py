5323301b3f379c91ac65c3b5d8cf6fc9dbe3a224
Kyle Dean <kdean003@ucr.edu>
2020-11-20 14:27:22 -0800

    implemented code to show calculation times on passing page

--- nfl_site/passing/views.py
+++ nfl_site/passing/views.py
@@ -10,6 +10,7 @@ import plotly
 import plotly.graph_objs as go
 import plotly.express as px
 import numpy as np
+import time
 
 _DATA_PATH = 'static/archive/'
 _DROP_FRAME = [ 
@@ -39,6 +40,8 @@ _RENAME_COLS = {
 
 pass_df = pd.DataFrame()
 players_df = pd.DataFrame()
+top_players_df = pd.DataFrame()
+previous_time = 0
 
 if pathlib.Path('static/archive/').exists():
     # Read associated csv files
@@ -48,11 +51,14 @@ if pathlib.Path('static/archive/').exists():
 
 # Create your views here.
 def pass_page(request):
-    global pass_df, players_df
+    global pass_df, players_df, previous_time
+
+    run_time = 0
 
     if pass_df.empty or players_df.empty or request.POST.get('Refresh Data') == 'Refresh Data':
         pass_df = csv_to_dict(f'{_DATA_PATH}passer.csv', 1)
         players_df = csv_to_dict(f'{_DATA_PATH}players.csv', 1)
+
 
     # Set holding variables
     form = forms.PassingForm()
@@ -66,9 +72,20 @@ def pass_page(request):
             context['analytics'] = analytics
 
     if request.POST.get('Show Table') == 'Show Table' or request.POST.get('Show Graph') == 'Show Graph' or request.POST.get('Show Scatter Plot') == 'Show Scatter Plot':
+        start_time = time.time()
         analytics = forms.PassingAnalytics(request.POST)
         if analytics.is_valid():
-            context = passing_analytics(analytics) 
+            top_player_count = analytics.cleaned_data.get('top_player_count')
+            if top_players_df.empty:
+                passing_analytics()
+            context = {'analytics': analytics, 'results': top_players_df.head(abs(top_player_count)), 'columns' : top_players_df.columns, 'n_count' : top_player_count}
+            run_time = time.time() - start_time
+            if previous_time != 0:
+                #difference = float(previous_time) - float(time)
+                context['previous_time'] = str(previous_time)
+                #context['difference'] = str(difference)
+            previous_time = run_time
+            context['time'] = str(run_time)
             context['form'] = form
             context['exists'] = True
 
@@ -168,11 +185,11 @@ def parse_form_entries(form):
     return {'form': form, 'results': results, 'columns' : results.columns}
 
 
-def passing_analytics(analytics):
-    top_player_count = analytics.cleaned_data.get('top_player_count')
+def passing_analytics():
+    global top_players_df
 
     # Get overall passing yards for each player without pandas
-    top_player_dict = top_n_passing_yards(top_player_count)
+    top_player_dict = top_n_passing_yards()
 
     r_dict={}
     temp_df = pd.DataFrame()
@@ -190,10 +207,11 @@ def passing_analytics(analytics):
 
     results.insert(loc=0, column='#', value=np.arange(start=1, stop=len(results)+1))
 
-    return {'analytics': analytics, 'results': results, 'columns' : results.columns, 'n_count' : top_player_count}
+    top_players_df = results
+
 
 # Analytics are done not using Pandas
-def top_n_passing_yards(tp_count):
+def top_n_passing_yards():
     global pass_df
     total_passing_dict = {}
     temp_df = pass_df[['playerId', 'passLength']]
@@ -212,13 +230,13 @@ def top_n_passing_yards(tp_count):
 
     passing_yards_desc = sorted(total_passing_dict.items(), key=itemgetter(1), reverse=True)
     # get the top n records from list and cast into a dictionary
-    n_records = dict(passing_yards_desc[:tp_count])
+    records = dict(passing_yards_desc)
 
     # Calculate average passing yards
-    for key, value in n_records.items():
+    for key, value in records.items():
         value[2] = round(value[0]/value[1], 3)
 
-    return n_records
+    return records
 
 
 def get_player_name(player_id):
@@ -227,7 +245,7 @@ def get_player_name(player_id):
 
 
 def add_passing_player(form):
-    global pass_df, players_df
+    global pass_df, players_df, top_players_df
 
     max_pass_id = int(pass_df['passId'].max())
     max_player_id = int(players_df['playerId'].max())
@@ -237,11 +255,11 @@ def add_passing_player(form):
     passing_outcome = form.cleaned_data.get('passing_outcome')
     passing_direction = form.cleaned_data.get('passing_direction')
     passing_depth = form.cleaned_data.get('passing_depth')
-    passing_length = str(form.cleaned_data.get('passing_length'))
+    passing_length = form.cleaned_data.get('passing_length')
 
-    player_name = player_name.split()
-    first_name = player_name[0]
-    last_name = player_name[1]
+    full_name = player_name.split()
+    first_name = full_name[0]
+    last_name = full_name[1]
 
     name_filter = players_df.loc[(players_df['nameFirst'] == first_name) & (players_df['nameLast'] == last_name)]
 
@@ -278,7 +296,7 @@ def add_passing_player(form):
                                'passOutcomes': passing_outcome,
                                'passDirection': passing_direction,
                                'passDepth': passing_depth,
-                               'passLength': passing_length
+                               'passLength': str(passing_length)
                                },
                                index=[0]
                                )
@@ -286,9 +304,32 @@ def add_passing_player(form):
     pass_df = pass_df.append(new_pass, ignore_index=True)
     pass_df = pass_df.reset_index(drop=True)
 
+    if not top_players_df.empty:
+        temp_df = top_players_df.loc[top_players_df['Player Name']==player_name]
+        if not temp_df.empty:
+            new_pass_length = temp_df['Total Passing Length (Yards)'].values[0] + int(passing_length)
+            new_times_passed = temp_df['Total Times Passed (from csv)'].values[0] + 1
+            
+            top_players_df.loc[top_players_df['Player Name']==player_name, ['Total Passing Length (Yards)', 'Total Times Passed (from csv)', 'Average Passing Length (Yards)']] = [new_pass_length, new_times_passed, round(new_pass_length/new_times_passed, 3)]
+            top_players_df = top_players_df.sort_values('Total Passing Length (Yards)', ascending=False)
+            
+        else:
+            new_top_n = pd.DataFrame({'Player Name': player_name,
+                                    'Total Passing Length (Yards)': passing_length,
+                                    'Total Times Passed (from csv)': 1,
+                                    'Average Passing Length (Yards)': round(passing_length/1, 3),
+
+                                    },
+                                    index=[0]
+                                    )
+            top_players_df = top_players_df.append(new_top_n, ignore_index=True)
+            top_players_df = top_players_df.sort_values('Total Passing Length (Yards)', ascending=False)
+
+        top_players_df = top_players_df.drop('#', 1)
+        top_players_df.insert(loc=0, column='#', value=np.arange(start=1, stop=len(top_players_df)+1))
 
 def delete_passing_player(form, full_delete = 0):
-    global pass_df, players_df
+    global pass_df, players_df, top_players_df
 
     # Get values from form
     player_name = form.cleaned_data.get('player_name').title()
@@ -297,9 +338,9 @@ def delete_passing_player(form, full_delete = 0):
     passing_depth = form.cleaned_data.get('passing_depth')
     passing_length = str(form.cleaned_data.get('passing_length'))
 
-    player_name = player_name.split()
-    first_name = player_name[0]
-    last_name = player_name[1]
+    full_name = player_name.split()
+    first_name = full_name[0]
+    last_name = full_name[1]
 
     name_filter = players_df.loc[(players_df['nameFirst'] == first_name) & (players_df['nameLast'] == last_name)]
 
@@ -314,6 +355,9 @@ def delete_passing_player(form, full_delete = 0):
             pass_df = pass_df.reset_index(drop=True)
             players_df = players_df.drop(name_filter.index)
             players_df = players_df.reset_index(drop=True)
+            if not top_players_df.empty:
+                top_players_df = top_players_df.drop(top_players_df.loc[top_players_df['Player Name']==player_name].index)
+                top_players_df = top_players_df.reset_index(drop=True)
 
         else:
             # If user selects passng outcome
@@ -334,3 +378,18 @@ def delete_passing_player(form, full_delete = 0):
 
             pass_df = pass_df.drop(results.index)
             pass_df = pass_df.reset_index(drop=True)
+
+            if not top_players_df.empty:
+                results['passLength'] = results['passLength'].astype(int)
+                results_total_passing_length = results['passLength'].sum()
+                results_count = len(results)
+                temp_df = top_players_df.loc[top_players_df['Player Name']==player_name]
+                new_pass_length = temp_df['Total Passing Length (Yards)'].values[0] - results_total_passing_length
+                new_times_passed = temp_df['Total Times Passed (from csv)'].values[0] - results_count
+                
+                top_players_df.loc[top_players_df['Player Name']==player_name, ['Total Passing Length (Yards)', 'Total Times Passed (from csv)', 'Average Passing Length (Yards)']] = [new_pass_length, new_times_passed, round(new_pass_length/new_times_passed, 3)]
+                top_players_df = top_players_df.sort_values('Total Passing Length (Yards)', ascending=False)
+
+        if not top_players_df.empty:
+            top_players_df = top_players_df.drop('#', 1)
+            top_players_df.insert(loc=0, column='#', value=np.arange(start=1, stop=len(top_players_df)+1))
